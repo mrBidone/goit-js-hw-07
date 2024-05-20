f7e65b54c8e4d28d16f48b5d333bf016ebe07fb9
mrBidone <ab.bz@ukr.net>
2024-05-20 22:13:30 +0200

    Add completed task #4 with styles

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const formELement = document.querySelector(".login-form");
+
+formELement.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const data = {
+        email: formELement.elements.email.value.trim(),
+        password: formELement.elements.password.value.trim(),
+    };
+    return data.email === "" || data.password === "" ?
+        alert("All form fields must be filled in") :
+        console.log(data),
+        formELement.reset();
+    
+});
+
 
