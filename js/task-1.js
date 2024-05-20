92e851bcb973f4295310be347c98548517a51e43
mrBidone <ab.bz@ukr.net>
2024-05-20 18:10:15 +0200

    Add comleted task #1

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+
+const categoriesData = categories.forEach(category => {
+    const categoryTitle = category.querySelector("h2").textContent;
+    console.log(`Category: ${categoryTitle}`);
+
+    const categoriesQuantity = category.querySelectorAll("li").length;
+    console.log(`Elements: ${categoriesQuantity}`);
+});
+
+
+
+
+
+
+
