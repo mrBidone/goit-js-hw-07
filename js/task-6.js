96d5e70719502f81adcf502ddab5abf4f0e9f756
mrBidone <ab.bz@ukr.net>
2024-05-22 10:58:07 +0200

    Add completed task#6 with styles

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,53 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputData = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const divForBoxes = document.querySelector("#boxes");
+const divForControls = document.querySelector("#controls");
+
+// console.log(createBtn.textContent)
+// console.log(destroyBtn.textContent)
+// console.log(divForBoxes)
+
+divForBoxes.classList.add("list-boxes");
+divForControls.classList.add("controls-container");
+createBtn.classList.add("btn-create");
+destroyBtn.classList.add("btn-destroy");
+inputData.classList.add("data-input");
+
+
+
+createBtn.addEventListener("click", () => {
+  const quantity = Number(inputData.value);
+  if (quantity >= 1 && quantity <= 100) {
+    createBoxes(quantity);
+    inputData.value = '';
+  }
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+  function destroyBoxes() {
+    divForBoxes.innerHTML = "";
+  };
+
+  function createBoxes(amount) {
+    destroyBoxes(); 
+
+    let size = 30;
+
+    for (let i = 0; i < amount; i++) {
+      const boxElem = document.createElement("div");
+      boxElem.style.minWidth = `${size}px`;
+      boxElem.style.height = `${size}px`;
+      size += 10;
+      boxElem.style.backgroundColor = getRandomHexColor();
+      divForBoxes.appendChild(boxElem);
+    };
+};
+  
+
+
