000b590525dc863a88487bae84b691be7ea9689e
Andrey Bidenko <79172596+mrBidone@users.noreply.github.com>
2024-05-25 10:10:33 +0200

    Update task-1.js
    Add example from Mentor-Grisha

--- js/task-1.js
+++ js/task-1.js
@@ -11,7 +11,11 @@ const categoriesData = categories.forEach(category => {
 
 
 
-
-
-
+// КОД ВІД ГРИГОРІЯ
+// const categoryList = document.querySelector('#categories');
+// console.log(`Number of categories: ${categoryList.children.length} `);
+// [...categoryList.children].forEach((item) => {
+//   console.log(`Category: ${item.firstElementChild.textContent}`);
+//   console.log(`Elements: ${item.lastElementChild.children.length}`);
+// })
 
